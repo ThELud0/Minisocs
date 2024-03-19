fb1c56528b9fb5705ccf0a32fd704c920ccdc6cf
Jun VILLETTE <jeanne.villette@telecom-sudparis.eu>
2024-03-19 19:16:53 +0100

    Avancement sur la classe Message

--- src/main/java/eu/telecomsudparis/csc4102/minisocs/Message.java
+++ src/main/java/eu/telecomsudparis/csc4102/minisocs/Message.java
@@ -1,6 +1,10 @@
 package eu.telecomsudparis.csc4102.minisocs;
 
+import java.time.Instant;
 import java.util.Objects;
+
+import eu.telecomsudparis.csc4102.util.Datutil;
+import eu.telecomsudparis.csc4102.util.OperationImpossible;
 
 public class Message {
 	
@@ -25,12 +29,72 @@ public class Message {
 	 * @param id l'id du message
 	 * @param contenu le contenu du message
 	 */
-	public Message(final String idMessage, final String contenu, final EtatMessage etatMessage) {
+	public Message(final String contenu, final EtatMessage etatMessage, String pseudoUtilisateur) {
 		if (contenu == null || contenu.isBlank()) {
 			throw new IllegalArgumentException("le contenu du message ne peut pas être null ou vide");	
 		}
-		this.idMessage=idMessage;
+		Instant instant = Datutil.instantDuTest(); // j'espère que ça fait ce qu'il faut lol
+		
+		this.idMessage=pseudoUtilisateur + Datutil.instantToString(instant);
 		this.contenu=contenu;
-		this.etatMessage=etatMessage;
-	}		
+		this.etatMessage=EtatMessage.ATTENTE;
+	}	
+	
+	public void moderer(EtatMessage etatMessage) throws OperationImpossible {
+		if (this.etatMessage != EtatMessage.ATTENTE) {
+			throw new OperationImpossible("seul un message en attente peut être modéré");
+		}
+		if ((etatMessage == EtatMessage.ATTENTE) || (etatMessage == EtatMessage.CACHE)) {
+			throw new OperationImpossible("modérer un message signifie l'accepter ou le refuser");
+		}
+		
+		this.etatMessage = etatMessage;
+		// A voir comment détruire le message si set à REFUSE
+	}
+	
+	public String getID() {
+		return idMessage;
+	}
+	
+	public String getContenu() {
+		return contenu;
+	}
+	
+	public EtatMessage getEtatMessage() {
+		return etatMessage;
+	}
+	
+	/**
+	 * vérifie l'invariant de la classe.
+	 * 
+	 * @return {@code true} si l'invariant est respecté.
+	 */
+	public boolean invariant() {
+		return idMessage != null && !idMessage.isBlank() && etatMessage != null && contenu !=null && !contenu.isBlank(); 
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(idMessage);
+	}
+	
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!(obj instanceof Message)) {
+			return false;
+		}
+		Message other = (Message) obj;
+		return Objects.equals(idMessage, other.idMessage);
+	}
+
+	@Override
+	public String toString() {
+		return "Message [idMessgae=" + idMessage + ", Contenu=" + contenu + ", État du message=" + etatMessage + "]";
+	}
+	
+
+	
 }	
